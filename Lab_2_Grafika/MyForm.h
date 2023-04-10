b2a55c201d9199185b67859bb97f39a5fa46a5bd
Egor5252 <123667969+Egor5252@users.noreply.github.com>
2023-04-10 23:11:04 +0300

    Добавьте файлы проекта.

--- Lab_2_Grafika/MyForm.h
+++ Lab_2_Grafika/MyForm.h
@@ -38,10 +38,18 @@ namespace Lab2Grafika {
 
 
 
-	private: System::Windows::Forms::Button^ button1;
+
 
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ X;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Y;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+
+
+
 
 	protected:
 
@@ -61,8 +69,13 @@ namespace Lab2Grafika {
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->X = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Y = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// dataGridView1
@@ -85,34 +98,103 @@ namespace Lab2Grafika {
 			this->Y->HeaderText = L"Y";
 			this->Y->Name = L"Y";
 			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::AppWorkspace;
+			this->pictureBox1->Location = System::Drawing::Point(263, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(382, 382);
+			this->pictureBox1->TabIndex = 3;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::pictureBox1_Paint);
+			this->pictureBox1->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseMove);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(527, 400);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(118, 20);
+			this->textBox1->TabIndex = 4;
+			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(263, 400);
+			this->button1->Location = System::Drawing::Point(263, 397);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 2;
+			this->button1->TabIndex = 5;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(361, 400);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 6;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(713, 82);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(123, 218);
+			this->richTextBox1->TabIndex = 7;
+			this->richTextBox1->Text = L"";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(637, 435);
+			this->ClientSize = System::Drawing::Size(848, 430);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->dataGridView1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
 			this->Name = L"MyForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
+
+	private: System::Void pictureBox1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+		Graphics^ g = e->Graphics;
+		
+	}
+
+	private: System::Void pictureBox1_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		this->textBox1->Text = "X = " + e->X.ToString() + "   Y = " + e->Y.ToString();
+	}
+
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		//this->textBox1->Text = this->dataGridView1->RowCount.ToString();
-		//this->textBox2->Text = (String^)this->dataGridView1->Rows[0]->Cells[0]->Value;
+		Graphics^ g = this->pictureBox1->CreateGraphics();
+		g->Clear(SystemColors::AppWorkspace);
+
+		Pen^ redPen = gcnew Pen(Color::Red);
+		redPen->Width = 1;
+
+		g->DrawLine(redPen, 0, 0, 100, 100);
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		auto A = this->dataGridView1->DataBindings;
+		auto a = A->GetEnumerator();
+		while (a->MoveNext()) {
+			this->richTextBox1->Text += a->Current->ToString();
+		}
 	}
 };
 }
+
+
+//this->textBox1->Text = this->dataGridView1->RowCount.ToString();
+//this->textBox2->Text = (String^)this->dataGridView1->Rows[0]->Cells[0]->Value;
